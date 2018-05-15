aa339d1ef68eb7c0d6b5bb2622745ed663cef248
adam-conway <adam.n.conway@gmail.com>
2018-05-15 15:13:58 -0600

    Added code to make sure a user coulnd't fire twice in a row

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -7,8 +7,12 @@ module Api
 
           turn_processor = TurnProcessor.new(game, params[:shot][:target])
 
-          turn_processor.run!
-          render json: game, message: turn_processor.message
+          if turn_processor.check_for_cheater(request.headers["X-API-key"])
+            render json: game, message: "Invalid move. It's your opponent's turn", status: 400
+          else
+            turn_processor.run!
+            render json: game, message: turn_processor.message
+          end
         end
       end
     end
