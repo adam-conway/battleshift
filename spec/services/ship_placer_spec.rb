d61e02ab0257cd0b488500448b0f379d80dfcd2a
adam-conway <adam.n.conway@gmail.com>
2018-05-19 14:47:47 -0600

    Finished test for placing a ship in row with empty spaces

--- spec/services/ship_placer_spec.rb
+++ spec/services/ship_placer_spec.rb
@@ -11,27 +11,29 @@ describe ShipPlacer do
     describe "testing" do
 
     it "exists when provided a board and ship" do
-      subect = ShipPlacer.new(game: @game, ship: create(:ship), start_space: "A1", end_space: "A3", api_key: @player_1.api_key)
-      expect(subject).to be_a ShipPlacer
+      ship_placer = ShipPlacer.new(game: @game, ship: create(:ship), start_space: "A1", end_space: "A3", api_key: @player_1.api_key)
+      expect(ship_placer).to be_a ShipPlacer
     end
 
-    xit "places the ship within a row with empty spaces" do
-      a1 = board.locate_space("A1")
-      a2 = board.locate_space("A2")
-      a3 = board.locate_space("A3")
-      b1 = board.locate_space("B1")
+    it "places the ship within a row with empty spaces" do
+      ship = create(:ship, length: 3)
 
-      expect(a1.contents).to be_nil
-      expect(a2.contents).to be_nil
-      expect(a3.contents).to be_nil
-      expect(b1.contents).to be_nil
+      a1 = @player_1_board.spaces.find_by(name: "A1")
+      a2 = @player_1_board.spaces.find_by(name: "A2")
+      a3 = @player_1_board.spaces.find_by(name: "A3")
+      b1= @player_1_board.spaces.find_by(name: "B1")
 
-      subject.run
+      expect(a1.ship).to be_nil
+      expect(a2.ship).to be_nil
+      expect(a3.ship).to be_nil
+      expect(b1.ship).to be_nil
 
-      expect(a1.contents).to eq(ship)
-      expect(a2.contents).to eq(ship)
-      expect(a3.contents).to be_nil
-      expect(b1.contents).to be_nil
+      ShipPlacer.new(game: @game, ship: ship, start_space: "A1", end_space: "A3", api_key: @player_1.api_key).run
+
+      expect(@player_1_board.spaces.find_by(name: "A1").ship).to eq(ship)
+      expect(@player_1_board.spaces.find_by(name: "A2").ship).to eq(ship)
+      expect(@player_1_board.spaces.find_by(name: "A3").ship).to eq(ship)
+      expect(@player_1_board.spaces.find_by(name: "B1").ship).to be_nil
     end
 
     xit "places the ship within a column with empty spaces" do
