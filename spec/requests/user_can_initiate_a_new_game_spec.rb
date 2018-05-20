d62395f07a39609966b129345b60a603c1073570
adam-conway <adam.n.conway@gmail.com>
2018-05-20 14:35:16 -0600

    Working on placing ship spec

--- spec/requests/user_can_initiate_a_new_game_spec.rb
+++ spec/requests/user_can_initiate_a_new_game_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 require_relative '../support/http_helpers.rb'
 
+
 describe 'Api::V1::GamesController' do
   context 'POST /api/v1/games' do
     include HTTPHelpers
 
     let (:challenger) { create(:user) }
     let (:opponent) { create(:user, email: 'nelson.jimmy@gmail.com', api_key: '987654321a') }
-    
+
     it 'creates a new game' do
       headers = { "CONTENT_TYPE" => "application/json", 'X-API-Key' => challenger.api_key }
 
